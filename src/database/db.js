65ef9ccef3e17a1f67faf04fa518d3e58feff907
Daniel Philipi <daniphilipi379@gmail.com>
2022-12-22 16:49:14 -0300

    feat: Estrutura inicial

---
+++ src/database/db.js
@@ -0,0 +1,13 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+export const DB = new Pool({
+
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+    
+})
