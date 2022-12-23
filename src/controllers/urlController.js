83cb343f339b284eb8d95e01c5c1e1e0faba0cbc
Daniel Philipi <daniphilipi379@gmail.com>
2022-12-23 08:59:23 -0300

    Feat: get url shorten

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -7,7 +7,7 @@ export async function shorten(req, res) {
 
     try{
         await DB.query(
-            "INSERT INTO urls ( url, shortUrl ) VALUES ( $1, $2 )",
+            "INSERT INTO urls ( url, short_url ) VALUES ( $1, $2 )",
             [ url, shortUrl ]
         );
 
@@ -32,7 +32,26 @@ export async function findUrl(req, res) {
 }
 
 export async function findShortUrl(req, res) {
-    
+    const { shorten } = req.params;
+    const urlExist = await DB.query(
+        "SELECT * FROM urls WHERE urls.short_url = $1",
+        [ shorten ]
+    );
+
+    if(!urlExist){
+        return res.sendStatus(404);
+    }
+
+    try{
+        await DB.query(
+            "INSERT INTO visits ( short_url, visit ) VALUES ( $1, $2 )",
+            [ shortUrl, 1 ]
+        );
+
+        res.redirect( urlExist.url );
+    }catch{
+        return res.sendStatus(500);
+    }
 }
 
 export async function deleteUrl(req, res) {
